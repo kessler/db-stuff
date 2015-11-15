932beaaaf308485d6602406a1a9dd21a9165c169
yaniv kessler <yanivk@gmail.com>
2015-11-15 21:02:52 +0200

    refactor, fix bugs, support insert commands without fields definition

--- test/BulkInsert.test.js
+++ test/BulkInsert.test.js
@@ -8,12 +8,12 @@ describe('BulkInsert', function () {
 	var insert, db;
 	it('creates a bulk insert query from an array of row data and executes it', function (done) {
 		insert.execute([
-			[1, 2, 3],
-			[4, 5, 6]
+			[1, 'g', 3],
+			[4, 'b', 6]
 		], function (err) {
 			expect(err).to.be.null;
 			expect(db.queries).to.have.length(1);
-			expect(db.queries).to.contain('insert into test (a,b,c) values  (1,2,3),\n(4,5,6)');
+			expect(db.queries).to.contain('insert into test (a,b,c) values (1,\'g\',3),(4,\'b\',6)');
 			done();
 		});
 	});
